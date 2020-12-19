7911c49dcf3e1892b3dacaf4af2ca1c2a355b88f
Ekaterina Lupynina <euveret@yandex.ru>
2020-12-19 22:37:26 +0500

    fix: доработать кнопки, структурировать код

--- script.js
+++ script.js
@@ -4,25 +4,19 @@ let popup = document.querySelector('.popup');
 let closeButton = popup.querySelector('.popup__close-button');
 let name = profile.querySelector('.profile__name');
 let job = profile.querySelector('.profile__job');
-let nameInput = popup.querySelector('.popup__text_name');
-let jobInput = popup.querySelector('.popup__text_job');
-
-nameInput.value = name.textContent;
-jobInput.value = job.textContent;
+let nameInput = popup.querySelector('.popup__text_type_name');
+let jobInput = popup.querySelector('.popup__text_type_job');
+let formPopup = popup.querySelector('.popup__form');
 
 function editFormOpener() {
+    nameInput.value = name.textContent;
+    jobInput.value = job.textContent;
     popup.classList.add('popup_opened');
 }
-
-editButton.addEventListener('click', editFormOpener);
 
 function editFormClosure() {
     popup.classList.remove('popup_opened');
 }
-
-closeButton.addEventListener('click', editFormClosure);
-
-let formPopup = popup.querySelector('.popup__form');
 
 function handleFormSubmit(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
@@ -30,9 +24,9 @@ function handleFormSubmit(evt) {
     // О том, как это делать, расскажем позже.
     name.textContent = nameInput.value;
     job.textContent = jobInput.value;
-    popup.classList.remove('popup_opened');
+    editFormClosure();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+editButton.addEventListener('click', editFormOpener);
+closeButton.addEventListener('click', editFormClosure);
 formPopup.addEventListener('submit', handleFormSubmit);
