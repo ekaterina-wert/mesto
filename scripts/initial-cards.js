9d5f959431c0781a56f2a7638c9b2f2019e259b9
Ekaterina Lupynina <euveret@yandex.ru>
2021-01-20 10:36:39 +0500

    refractor: division of functions

---
+++ scripts/initial-cards.js
@@ -0,0 +1,25 @@
+const initialCards = [{
+        name: 'Челябинск',
+        link: './images/chelyabinsk.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: './images/ivanovo.jpg'
+    },
+    {
+        name: 'Самара',
+        link: './images/samara.jpg'
+    },
+    {
+        name: 'Москва',
+        link: './images/moscow.jpg'
+    },
+    {
+        name: 'Казань',
+        link: './images/kazan.jpg'
+    },
+    {
+        name: 'Владивосток',
+        link: './images/vladivostok.jpg'
+    }
+];
